7fdc461836f01e77b5d1e6f90b667e13e8edbc70
Mikhail <79122806999@ya.ru>
2023-05-20 18:49:01 +0500

    Tasks_Done_4

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -11,5 +11,8 @@ int num2 = Convert.ToInt32 (Console.ReadLine());
 int max = num1;
 if (num1>max) max = num1;
 if (num2>max) max = num2;
+int min = num1;
+if (num1<min) min = num1;
+if (num2<min) min = num2;
 
-System.Console.WriteLine("Больше число "+max);
+System.Console.WriteLine("Больше число "+max+", а меньшее число "+min);
