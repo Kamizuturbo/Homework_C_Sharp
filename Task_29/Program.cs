3fc24e5e5dcea4331ff72e35b5bb741360dde631
Mikhail <79122806999@ya.ru>
2023-06-03 12:07:51 +0500

    Tasks

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -6,3 +6,18 @@
 
 // 9012 -> 12
 
+int SumAllDigit (int num1)
+{
+    int result = 0;
+    while (num1>0)
+    {
+        result+=num1%10;
+        num1=num1/10;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+System.Console.WriteLine(SumAllDigit(num1));
