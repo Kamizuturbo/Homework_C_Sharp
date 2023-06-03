61e284deec82b4116921940dce24e7acdb3f21a8
Mikhail <79122806999@ya.ru>
2023-06-03 11:55:15 +0500

    Tasks

---
+++ Task_25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// // 2, 4 -> 16
+
+double Power (double num1,double num2)
+{
+    double PowerNum1=Convert.ToDouble(Math.Pow(num1,num2));
+    return PowerNum1;
+}
+Console.Clear();
+Console.WriteLine("Введите число А");
+double num1 = Convert.ToDouble (Console.ReadLine());
+Console.WriteLine("Введите число B");
+double num2 = Convert.ToDouble (Console.ReadLine());
+System.Console.WriteLine(Power(num1,num2));
---
+++ Task_27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// // 6, 1, 33 -> [6, 1, 33]
+
+void Arr(int[] Array)
+{
+    for (int i = 0; i<Array.Length; i++)
+    {
+         Array[i] = new Random().Next(0, 100);
+         System.Console.Write($"{Array[i]},");
+    }
+}
+
+Console.Clear();
+int[] Array = new int[new Random().Next(0, 9)];
+Arr(Array);
