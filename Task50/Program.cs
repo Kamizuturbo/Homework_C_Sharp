7c73b085aee558be65cf08c83037de8d79c1c4a3
Mikhail <79122806999@ya.ru>
2023-06-12 14:22:35 +0500

    Tasks

---
+++ Task50/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает значение элемента 
+// в двумерном массиве, и возвращает позицию этого элемента или же указание, что такого элемента нет.
+
+void FillArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j]= new Random().Next(-30,31);
+}
+
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j], 3}  ");//3=резер кол-ва символов для вывода значения
+        Console.WriteLine();
+        }
+}
+
+void Search(int [,] array,int SearchedNum)
+{       bool Find = false;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (array[i,j]==SearchedNum)
+                    System.Console.WriteLine($"Координаты вашего числа {i},{j}");
+                    Find = true;
+            }
+        }
+        if (Find==false) System.Console.WriteLine("Вашего числа нет в массиве");
+}
+
+// void Search (int[,] array, int SearchedNum)
+// {
+//     bool Find = false;
+//     foreach (var item in array)
+//     {
+//         if (item == SearchedNum)
+//         {
+//             System.Console.WriteLine("Да, есть");
+//             Find=true;
+//             break;
+//         }
+//     }
+//     if (Find == false) System.Console.WriteLine("Вашего числа нет в массиве");
+// }
+
+Console.Clear();
+System.Console.WriteLine("Введите кол-во строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите кол-во столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array=new int[rows, cols];
+
+FillArray(array);
+PrintArray(array);
+
+System.Console.WriteLine("Введите значение искомого элемента");
+int SearchedNum = Convert.ToInt32(Console.ReadLine());
+
+Search(array, SearchedNum);
+
---
+++ Task52/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом 
+// столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void FillArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j]= new Random().Next(1,10);
+}
+
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j], 3}  ");//3=резер кол-ва символов для вывода значения
+        Console.WriteLine();
+        }
+}
+
+int[] TraceArray(int[,] array)
+{
+    int[] summ = new int[array.GetLength(1)];
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        int index=0;
+        for (int j = 0; j < array.GetLength(1); j++)
+            {
+            {
+                summ[index]=(summ[index]+array[i,j]);
+                index++;
+            }           
+        }
+        }
+    return summ;
+}
+
+double PrintArrayOdnomer(int[] array)
+{
+    double num=array.Length;
+    foreach (var item in array)
+    {
+        Console.Write($"{Math.Round(item/num,3), 3} ");
+    }
+    System.Console.WriteLine();
+    return num;
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите кол-во строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите кол-во столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array=new int[rows, cols];
+
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+int[] summ = TraceArray(array);
+PrintArrayOdnomer(summ);
